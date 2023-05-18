b798d574b6ce24556cd2d21172b8e1e03f7fc123
Hossein Naderi <hossein-naderi@hotmail.com>
2023-05-18 20:54:29 +0330

    Http4s jdk client accept java.net.http....Builder

--- modules/http4s-jdk/src/main/scala/JDKKubernetesClient.scala
+++ modules/http4s-jdk/src/main/scala/JDKKubernetesClient.scala
@@ -19,34 +19,38 @@ package http4s
 
 import cats.effect.kernel.Async
 import cats.effect.kernel.Resource
+import cats.effect.std.Env
 import cats.syntax.all._
+import fs2.io.file.Files
 import org.http4s.client.Client
 import org.http4s.jdkhttpclient.JdkHttpClient
 
 import java.net.http
+import java.net.http.HttpClient.Builder
 import javax.net.ssl.SSLContext
-import cats.effect.std.Env
-import fs2.io.file.Files
 
-final class JDKKubernetesClient[F[_]: Async: Files: Env]
-    extends JVMPlatform[F] {
+final class JDKKubernetesClient[F[_]: Async: Files: Env] private (
+    builder: Builder
+) extends JVMPlatform[F] {
 
-  override protected def buildClient: Resource[F, Client[F]] =
-    Resource.eval(JdkHttpClient.simple[F])
+  override protected def buildClient: Resource[F, Client[F]] = from(b => b)
 
   override protected def buildWithSSLContext
       : SSLContext => Resource[F, Client[F]] = ssl =>
-    Resource.eval(Async[F].executor.flatMap { exec =>
+    from(_.sslContext(ssl)).preAllocate(
       Async[F].delay {
-        val builder = http.HttpClient.newBuilder()
         // workaround for https://github.com/http4s/http4s-jdk-http-client/issues/200
         if (Runtime.version().feature() == 11) {
           val params = ssl.getDefaultSSLParameters()
           params.setProtocols(params.getProtocols().filter(_ != "TLSv1.3"))
         }
+      }
+    )
 
-        val client = builder
-          .sslContext(ssl)
+  private def from(customize: Builder => Builder) =
+    Resource.eval(Async[F].executor.flatMap { exec =>
+      Async[F].delay {
+        val client = customize(builder)
           .executor(exec)
           .build()
 
@@ -54,4 +58,12 @@ final class JDKKubernetesClient[F[_]: Async: Files: Env]
       }
     })
 
+}
+
+object JDKKubernetesClient {
+  def apply[F[_]: Async: Files: Env]: JDKKubernetesClient[F] =
+    new JDKKubernetesClient[F](http.HttpClient.newBuilder())
+
+  def apply[F[_]: Async: Files: Env](builder: Builder): JDKKubernetesClient[F] =
+    new JDKKubernetesClient[F](builder)
 }
