6eef01cdc0a545a8c01b7cc1631fabf0f4fc63a8
Hossein Naderi <hossein-naderi@hotmail.com>
2026-05-25 11:24:59 +0330

    Fixed wrong ZIO stream handling

--- modules/zio/src/main/scala/ZIOExecBackend.scala
+++ modules/zio/src/main/scala/ZIOExecBackend.scala
@@ -34,23 +34,37 @@ class ZIOExecBackend(client: Client)
       case _                 => url.scheme(Scheme.WSS)
     }
 
+  private def decodeFrame(data: Chunk[Byte]): Option[ExecEvent] =
+    if (data.isEmpty) None
+    else
+      (data.head.toInt & 0xff) match {
+        case 1 => Some(ExecEvent.Stdout(data.drop(1).toArray))
+        case 2 => Some(ExecEvent.Stderr(data.drop(1).toArray))
+        case 3 =>
+          val str = new String(data.drop(1).toArray, "UTF-8")
+          JsonDecoder[v1.Status]
+            .decodeJson(str)
+            .toOption
+            .map(ExecEvent.Error(_))
+        case _ => None
+      }
+
   override def execConnect(
       url: String,
       headers: Seq[(String, String)],
       params: Seq[(String, String)],
       cookies: Seq[(String, String)]
   ): ZKStream[ExecInput] => ZKStream[ExecEvent] = { input =>
-    type WsChannel =
-      Channel[ChannelEvent[WebSocketFrame], ChannelEvent[WebSocketFrame]]
-
-    ZStream.asyncScoped[Any, Throwable, ExecEvent] { emit =>
+    ZStream.unwrapScoped[Any] {
       for {
         wsUrl <- urlFor(url, params).map(rewriteToWs)
         wsHeaders = Headers("Sec-WebSocket-Protocol" -> "v4.channel.k8s.io") ++
           headersFor(headers) ++ cookiesFor(cookies)
 
-        handler = Handler.fromFunctionZIO[WsChannel] { channel =>
-          val send = input
+        events <- Queue.unbounded[Take[Throwable, ExecEvent]]
+
+        handler = Handler.webSocket { channel =>
+          val sendInput = input
             .mapZIO[Any, Throwable, Unit] {
               case ExecInput.Stdin(bytes) =>
                 channel.send(
@@ -73,44 +87,32 @@ class ZIOExecBackend(client: Client)
             }
             .runDrain
 
-          val receive = channel
-            .receiveAll {
-              case ChannelEvent.Read(WebSocketFrame.Binary(data))
-                  if data.nonEmpty =>
-                (data.head.toInt & 0xff) match {
-                  case 1 =>
-                    val tail = data.drop(1).toArray
-                    ZIO.succeed(
-                      emit(ZIO.succeed(Chunk.single(ExecEvent.Stdout(tail))))
-                    )
-                  case 2 =>
-                    val tail = data.drop(1).toArray
-                    ZIO.succeed(
-                      emit(ZIO.succeed(Chunk.single(ExecEvent.Stderr(tail))))
-                    )
-                  case 3 =>
-                    val str = new String(data.drop(1).toArray, "UTF-8")
-                    ZIO.succeed(
-                      JsonDecoder[v1.Status].decodeJson(str).foreach { s =>
-                        emit(ZIO.succeed(Chunk.single(ExecEvent.Error(s))))
-                      }
-                    )
-                  case _ => ZIO.unit
-                }
-              case ChannelEvent.Unregistered =>
-                ZIO.succeed(emit(ZIO.fail(None)))
-              case _ => ZIO.unit
-            }
-            .ensuring(ZIO.succeed(emit(ZIO.fail(None))))
+          val receiveOutput = channel.receiveAll {
+            case ChannelEvent.Read(WebSocketFrame.Binary(data)) =>
+              decodeFrame(data) match {
+                case Some(e) => events.offer(Take.single(e)).unit
+                case None    => ZIO.unit
+              }
+            case ChannelEvent.ExceptionCaught(cause) => ZIO.fail(cause)
+            case _                                   => ZIO.unit
+          }
 
-          send.race(receive)
+          ZIO
+            .scoped[Any] {
+              sendInput.forkScoped *> receiveOutput
+            }
+            .onExit { exit =>
+              events.offer(exit.foldExit(Take.failCause(_), _ => Take.end))
+            }
         }
 
-        _ <- WebSocketApp(handler)
+        _ <- handler
           .connect(wsUrl, wsHeaders)
           .provideSomeEnvironment[Scope](_.add[Client](client))
+          .tapErrorCause(c => events.offer(Take.failCause(c)).unit)
           .forkScoped
-      } yield ()
+
+      } yield ZStream.fromQueue(events).flattenTake
     }
   }
 }
